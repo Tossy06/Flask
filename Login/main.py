45ec4fe040193036294e9b6b8ea39b4765fe3305
David Gomez <davidgomez.sp06@gmail.com>
2024-08-17 14:06:29 -0500

    Solved bug

--- Login/main.py
+++ Login/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, redirect, url_for, render_template, session, flash
 import psycopg2
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from config import Config
+
 app = Flask(__name__)
 app.secret_key = 'supersecretKey'
 
@@ -9,38 +10,31 @@ def get_db_connection():
     conn = psycopg2.connect(**Config.DATABASE)
     return conn
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         conne = get_db_connection()
         cur = conne.cursor()
-        try:
-            cur.execute(f'SELECT *FROM users WHERE username=%s AND password=%s',(username, password))
-        except psycopg2.IntegrityError:
-            conne.rollback()
-            flash('Username already exists', 'danger')
-            
-        
+
+        cur.execute('SELECT * FROM users WHERE username = %s', (username,))
         user = cur.fetchone()
         cur.close()
         conne.close()
-        
-        if user:
-             # Si el usuario existe, se inicia sesión
+
+        if user and check_password_hash(user[2], password):  # Asume que la contraseña está en la tercera columna
             session['username'] = username
             flash('Login successful!', 'success')
-            return redirect(url_for('index.html'))
+            return redirect(url_for('index'))
         else:
-            flash('Invalid username or password', 'danger')
+            return render_template('error.html')
         
     return render_template('login.html')
-
-
-
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -50,17 +44,20 @@ def register():
         hashed_password = generate_password_hash(password)
         conne = get_db_connection()
         cur = conne.cursor()
+
         try:
-            cur.execute('INSERT INTO users (username, password) VALUES (%s,%s)', (username, hashed_password))
+            cur.execute('INSERT INTO users (username, password) VALUES (%s, %s)', (username, hashed_password))
             conne.commit()
+            flash('Registration successful', 'success')
+            return redirect(url_for('login'))
         except psycopg2.IntegrityError:
             conne.rollback()
             flash('Username already exists', 'danger')
+            return redirect(url_for('register'))
         finally:
             cur.close()
             conne.close()
-        flash('Registration successful')
-        
-    return render_template ('register.html')
 
-app.run(host='0.0.0.0', port='3050', debug= True)
+    return render_template('register.html')
+
+app.run(host='0.0.0.0', port=3050, debug=True)
