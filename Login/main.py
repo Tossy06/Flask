0186b88c37f9798b416149942e39377b8e7516f2
David Gomez <davidgomez.sp06@gmail.com>
2024-08-17 00:14:05 -0500

    First Part of The Login

--- Login/main.py
+++ Login/main.py
@@ -9,6 +9,39 @@ def get_db_connection():
     conn = psycopg2.connect(**Config.DATABASE)
     return conn
 
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conne = get_db_connection()
+        cur = conne.cursor()
+        try:
+            cur.execute(f'SELECT *FROM users WHERE username=%s AND password=%s',(username, password))
+        except psycopg2.IntegrityError:
+            conne.rollback()
+            flash('Username already exists', 'danger')
+            
+        
+        user = cur.fetchone()
+        cur.close()
+        conne.close()
+        
+        if user:
+             # Si el usuario existe, se inicia sesión
+            session['username'] = username
+            flash('Login successful!', 'success')
+            return redirect(url_for('index.html'))
+        else:
+            flash('Invalid username or password', 'danger')
+        
+    return render_template('login.html')
+
+
+
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
